de2eb130fa933d968e7cfe90bae5c10164fa8138
geraldsamosir <gerald.unforgiven@gmail.com>
2020-11-27 15:45:18 +0700

    integrated minus styling

--- db/profile.js
+++ db/profile.js
@@ -1,8 +1,9 @@
 export default ()=> ({
   "name": "GERALD HALOMOAN SAMOSIR",
-  "profile_url": "",
-  "profile_hover_url": "",
-  "Moto": "",
+  "profile_url": "https://avatars3.githubusercontent.com/u/7693125?s=460&u=2d77923712ec70fbec6467aedc96912b8f5d51fa&v=4s",
+  "profile_hover_url": "https://media-exp1.licdn.com/dms/image/C5103AQH6EdyB2IyE3g/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=-zPpX8uDzXFXSHNYKET0JPnpPgGyPZ0fUS0YisvodjA",
+  "Moto": "Simple is Better",
+  "MotoItem": "Hi i'm Gerald halomoan Samosir,  \n i have experience to build some app and website in financial serives and video and large stream file \n  as a senior developer i have many experience to solve for scaling dan develop large and small apps and website. \n if you want to make an app or website you can contact me and we will improve that to be better",
   "Education": [
     {
       "id": "1",
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,105 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Gerald Halomoan Samosir</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+        <h1>{props.profile.data.name}</h1>
+        <img src={props.profile.data.profile_url}/>
+        <img src={props.profile.data.profile_hover_url}/>
+        <h3>{props.profile.data.Moto}</h3>
+        <span>{props.profile.data.MotoItem}</span>
+        <h3>
+          Education
+        </h3>
+        <ul>
+          {
+            props.profile.data.Education.map(d=>{
+              return(
+                <li>
+                  <div>
+                     {d.name} <br/>
+                     {d.major} <br/>
+                     {d.dateRange}<br/>
+                  </div>
+                </li>
+              )
+            })
           }
+        </ul>
+        <h3>Certification</h3>
+        <ul>
+          {
+            props.profile.data.Certificaion.map(d=>{
+              return(
+                <li>
+                  <div>
+                     {d.name} <br/>
+                     {d.institution} <br/>
+                     {
+                       d.credentialType === 'Link'?
+                        <a href={d.credential}>Lihat Credential</a>
+                        : <span>{d.credential}</span>
+                     }
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <h3>Jobs Experience</h3>
+        <ul>
+        {
+          props.Jobs.data.jobs.map((d)=>{
+            return(
+            <li>
+              <div>
+                 {d.name} <br/>
+                 {d.joinDate} - {d.exitDate === null ? 'Present': d.exitDate} <br/>
+                 {d.Roles} <br/>
+                 {d.JobDesc}
+              </div>
+            </li>)
+          })
         }
-      `}</style>
+        </ul>
+        <h3>Portfolio</h3>
+        <ul>
+        {
+          props.portfolio.data.projects.map((d)=>{
+            return(
+              <li>
+                <div>
+                   <img src={d.covers['115']}/> <br/>
+                   <span>{d.name}</span>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+                </div>
+              </li>
+            )
+          })
         }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+        </ul>
+     </div>
   )
+}
+
+
+export async function getServerSideProps(ctx) {
+  const baseUrl =  process.env.env !== 'production'? process.env.API_URL_DEV :process.env.API_URL_PROD
+  let profile  = await fetch(`${baseUrl}/profile`);
+  profile = await profile.json();
+  let Jobs = await fetch(`${baseUrl}/jobs`)
+  Jobs = await Jobs.json()
+  let portfolio = await fetch(`${baseUrl}/portfolio`)
+  portfolio = await portfolio.json()
+  return {
+    props: {
+      profile,
+      Jobs,
+      portfolio
+    }
+  }
 }
